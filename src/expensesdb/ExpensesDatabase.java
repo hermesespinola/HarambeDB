582d7f9663ad8435ec1b7bc83a6c9139a6d82c3c
Hermes Espínola <hermesespinola@gmail.com>
2016-11-29 22:03:22 -0600

    remove structures from documentation and added ExpensesDatabase

--- src/expensesdb/ExpensesDatabase.java
+++ src/expensesdb/ExpensesDatabase.java
@@ -1,8 +1,11 @@
-import java.util.Comparator;
+package expensesdb;
+
 import database.table.relation.Relation;
 import database.table.column.Column;
+import database.HarambException;
 import database.table.row.Row;
 import database.table.Table;
+import java.util.Comparator;
 import structures.list.List;
 import database.Database;
 
@@ -89,8 +92,9 @@ public final class ExpensesDatabase {
 	* Adds an item to the items table
 	* @param	itemName		The name of the item to add
 	* @param	itemExpense	The expense of the item to add
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static void addItem(String itemName, Integer itemExpense) throws Exception {
+	public static void addItem(String itemName, Integer itemExpense) throws HarambException {
 		items.addRow(itemName).set(items.getColumn("Expense"), itemExpense);
 	}
 
@@ -99,8 +103,9 @@ public final class ExpensesDatabase {
 	* @param	name				The name of the item to add
 	* @param	address			The address of the user
 	* @param invoices			The invoices the user has
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static void addUser(String name, String address, Integer[] invoices) throws Exception {
+	public static void addUser(String name, String address, Integer[] invoices) throws HarambException {
 		users.addRow(name).set(users.getColumn("Address"), address).set(users.getColumn("Invoices"), invoices);
 	}
 
@@ -108,8 +113,9 @@ public final class ExpensesDatabase {
 	* Adds an user to the users table
 	* @param	name				The name of the item to add
 	* @param	address			The address of the user
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static void addUser(String name, String address) throws Exception {
+	public static void addUser(String name, String address) throws HarambException {
 		users.addRow(name).set(users.getColumn("Address"), address).set(users.getColumn("Invoices"), new Integer[1]);
 	}
 
@@ -117,8 +123,9 @@ public final class ExpensesDatabase {
 	* Removes an user from the users table
 	* @param	userName					The name of the item to add
 	* @param	removeInvoices		Tells if the invoices related to the user should be removed as well
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static void delteUser(String userName, boolean removeInvoices) throws Exception {
+	public static void delteUser(String userName, boolean removeInvoices) throws HarambException {
 		if (removeInvoices) {
 			Integer[] uids = users.getRow(userName).get(users.getColumn("Invoices"));
 			for (Integer invoiceId : uids) {
@@ -131,16 +138,18 @@ public final class ExpensesDatabase {
 	/**
 	* Removes an invoice from the invoices table
 	* @param	invoiceUID		The number of invoice to remove
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static void deleteInvoice(Integer invoiceUID) throws Exception {
+	public static void deleteInvoice(Integer invoiceUID) throws HarambException {
 		invoices.removeRow(invoiceUID);
 	}
 
 	/**
 	* Removes an item from the items table
 	* @param	itemName	The name of the item to remove
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static void deleteItem(String itemName) throws Exception {
+	public static void deleteItem(String itemName) throws HarambException {
 		items.removeRow(itemName);
 	}
 
@@ -148,8 +157,9 @@ public final class ExpensesDatabase {
 	* Retrieves the user row with the given name
 	* @param	name	The name of the user
 	* @return				The user row with the given name
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static Row getUser(String name) throws Exception {
+	public static Row getUser(String name) throws HarambException {
 		return users.getRow(name);
 	}
 
@@ -157,8 +167,9 @@ public final class ExpensesDatabase {
 	* Retrieves the user row and its invoices
 	* @param	name		The name of the item to add
 	* @return					The user row and its invoices where list[0] is the user row and list[1:n] is the list of invoices
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static List<Row> getUserAndInvoices(String name) throws Exception {
+	public static List<Row> getUserAndInvoices(String name) throws HarambException {
 		return users.getRowWithRelation(name, db);
 	}
 
@@ -166,8 +177,9 @@ public final class ExpensesDatabase {
 	* Retrieves the user row, its invoices and its items
 	* @param	name		The name of the item to add
 	* @return					the user row, its invoices and its items
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static List<Row> getUserAndExpenses(String name) throws Exception {
+	public static List<Row> getUserAndExpenses(String name) throws HarambException {
 		return users.getRowWithRelations(name, db);
 	}
 
@@ -175,8 +187,9 @@ public final class ExpensesDatabase {
 	* Retrieves an invoice from the invoices table
 	* @param	invoiceUID		The invoice number
 	* @return								The row of the invoice
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static Row getInvoice(Integer invoiceUID) throws Exception {
+	public static Row getInvoice(Integer invoiceUID) throws HarambException {
 		return invoices.getRow(invoiceUID);
 	}
 
@@ -184,8 +197,9 @@ public final class ExpensesDatabase {
 	* Retrieves the invoice row and its items
 	* @param	invoiceUID	The invoice number
 	* @return							The invoice row and its items where list[0] is the invoice row and list[1:n] is the list of items
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static List<Row> getInvoiceAndItems(Integer invoiceUID) throws Exception {
+	public static List<Row> getInvoiceAndItems(Integer invoiceUID) throws HarambException {
 		return invoices.getRowWithRelations(invoiceUID, db);
 	}
 
@@ -194,19 +208,21 @@ public final class ExpensesDatabase {
 	* Retrieves the item row with the given name
 	* @param	itemName	The name of the item
 	* @return						The item row with the given name
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static Row getItem(String itemName) throws Exception {
+	public static Row getItem(String itemName) throws HarambException {
 		return items.getRow(itemName);
 	}
 
 	/**
 	* Calculates how similar are two users by expense, similarity is defined as
 	*	the absolute value of the difference of the expenses of the two users
-	* @param	xname			The name of an user
+	* @param	xName			The name of an user
 	* @param	yName			The name of another user
 	* @return						The similarity between the users
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public int userSimilarity(String xName, String yName) throws Exception {
+	public int userSimilarity(String xName, String yName) throws HarambException {
 		return Math.abs(getTotalPayments(xName) - getTotalPayments(yName));
 	}
 
@@ -214,8 +230,9 @@ public final class ExpensesDatabase {
 	* Retrieves the total payments of an user
 	* @param	userName	The name of the user
 	* @return						The sum of all his/her expenses payments
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public int getTotalPayments(String userName) throws Exception {
+	public int getTotalPayments(String userName) throws HarambException {
 		List<Row> userInvoices = users.getRowWithRelation(userName, db);
 		int total = 0;
 		for (int i = 1; i < userInvoices.size(); i++) {
@@ -230,8 +247,9 @@ public final class ExpensesDatabase {
 	* @param	userName		The name of the user this invoice belongs to
 	* @param	invoiceUID	The invoice number of the this invoice
 	* @param	itemsNames	The list of items this invoice have
+	*	@throws HarambException		If there is some error reading or writing to the database
 	*/
-	public static void addInvoice(String userName, Integer invoiceUID, String[] itemsNames) throws Exception {
+	public static void addInvoice(String userName, Integer invoiceUID, String[] itemsNames) throws HarambException {
 		Row userRow = users.getRow(userName);
 		if (userRow == null) {
 			throw new RuntimeException("No such user: " + userName);
@@ -251,7 +269,7 @@ public final class ExpensesDatabase {
 		userRow.set(users.getColumn("Invoices"), newInvoices);
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws HarambException {
 		addUser("Hermes", "Aqui");
 		addUser("Mike", "Allá");
 		addUser("Eros", "Aquí también");
